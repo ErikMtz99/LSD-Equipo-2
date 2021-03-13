7e0ada41009df1f97e7d8d6c62288db5be8d5318
AlexNaunt5 <42699230+AlexNaunt5@users.noreply.github.com>
2021-03-12 20:17:03 -0600

    final reloj

--- Practica5/reloj.vhd
+++ Practica5/reloj.vhd
@@ -24,47 +24,9 @@ use IEEE.STD_LOGIC_1164;
 entity Reloj is
         Port(clk_in: in bit;
         --
-        D7A : out STD_LOGIC_VECTOR (7 downto 0);
-        D71 : out STD_LOGIC_VECTOR (7 downto 0)
-        
+        seg7d, an7d : out bit_vector(7 downto 0)
         );
 end Reloj;
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_1164;
-
-
-entity decoder7 is
-Port(
-        Clk: in bit;
-        N1: in bit_vector(3 downto 0);
-        S82: out bit_vector(7 downto 0)
-);
-end decoder7;
-
-architecture Behavioral of decoder7 is
-
-begin
-Process(Clk)
-begin
- 
--- pasa valor de 4 bits a su equivalente para display de 7 segmentos
-case (N1) is
-                    when "0000" => S82 <= "11000000";
-                    when "0001" => S82 <= "11111001";
-                    when "0010" => S82 <= "10100100";
-                    when "0011" => S82 <= "10110000";
-                    when "0100" => S82 <= "10011001";
-                    when "0101" => S82 <= "10010010";
-                    when "0110" => S82 <= "10000010";
-                    when "0111" => S82 <= "11111000";
-                    when "1000" => S82 <= "10000000";
-                    when "1001" => S82 <= "10010000";
-                    when others => S82 <= "11111111";
-                    end case;
-end process;
-end Behavioral;
 
 
 architecture Behavioral of Reloj is
@@ -90,14 +52,14 @@ architecture Behavioral of Reloj is
         Port( Clk: in bit;
               N1: in bit_vector(3 downto 0);
               S82: out bit_vector(7 downto 0)
-            );
-    end component;
+        );
+end component;
     
-    component seg7m
-        Port(ck : in  std_logic;                          -- 100MHz system clock
-             number : in  std_logic_vector (63 downto 0); -- eight digit number to be displayed
-             seg : out  std_logic_vector (7 downto 0);    -- display cathodes
-             an : out  std_logic_vector (7 downto 0));    -- display anodes (active-low, due to transistor complementing));
+    component segm7
+        Port(ck : in  bit;                          -- 100MHz system clock
+             number : in  bit_vector (63 downto 0); -- eight digit number to be displayed
+             seg : out  bit_vector (7 downto 0);    -- display cathodes
+             an : out  bit_vector (7 downto 0));    -- display anodes (active-low, due to transistor complementing));
     end component;
 
 signal startHrsCounter: bit;
@@ -111,10 +73,11 @@ signal hrs_dec: bit;
 signal hrs_uni : bit_vector(3 downto 0); 
 signal min_uni, seg_uni: bit_vector(3 downto 0);
 signal min_dec, seg_dec: bit_vector(2 downto 0);
-
+signal D7A : BIT_VECTOR (7 downto 0);
+signal D71 : BIT_VECTOR (7 downto 0);
 --------
 -- signal watch: std_logic;
--- signal d7s : STD_LOGIC_VECTOR (63 downto 0) := "1111111111111111111111111111111100000000000000000000000000000000";
+signal d7s : bit_VECTOR (63 downto 0):= "1111111111111111111111111111111100000000000000000000000000000000";
 
 
 begin
@@ -143,17 +106,18 @@ begin
     horas <= "000" & hrs_dec;
     minutos <=  '0' & internal_min_dec;
  
-    DISP: decoder7 port map (clk1hz, minutos, min_dec_disp);
-    DISP2: decoder7 port map (clk1hz, internal_min_uni, min_uni_disp);
-    DISP3: decoder7 port map (clk1hz, hrs_uni, hrs_uni_disp);
-    DISP4: decoder7 port map (clk1hz, horas, hrs_dec_disp);
-   
+DISP: decoder7 port map (clk1hz, minutos, min_dec_disp);
+DISP2: decoder7 port map (clk1hz, internal_min_uni, min_uni_disp);
+DISP3: decoder7 port map (clk1hz, hrs_uni, hrs_uni_disp);
+DISP4: decoder7 port map (clk1hz, horas, hrs_dec_disp);
+
+SEGMENTOS : segm7 port map (ck => clk1hz, number => d7s, seg => seg7d, an => an7d);
    -- watch <= to_std_logic(clk_in);
-   --d7s(7 downto 0) <= to_std_logic(min_uni_disp);
-    --d7s(15 downto 8) <= min_dec_disp;
-   -- d7s(23 downto 16) <= hrs_uni_disp
-   -- d7s(31 downto 24) <= hrs_dec_disp
-   -- Prueba: seg7m port map(watch, d7s, D7A, D71)
+   d7s(7 downto 0) <= min_uni_disp;
+   d7s(15 downto 8) <= min_dec_disp;
+   d7s(23 downto 16) <= hrs_uni_disp;
+   d7s(31 downto 24) <= hrs_dec_disp;
+--   Prueba: segm7 port map(clk1hz, d7s, D7A, D71);
    
 end Behavioral;
 
